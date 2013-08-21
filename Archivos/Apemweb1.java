9d25930aebb217b45aae8829a374bc6a6d8696f0
Serqueg10 <sergiogr9506@gmail.com>
2013-08-21 10:50:39 -0500

    cree el menu y agrege resta

--- Archivos/Apemweb1.java
+++ Archivos/Apemweb1.java
@@ -15,18 +15,53 @@ public class Apemweb1 {
     int a;
     int b;
     int c;
+    int resta;
+    
 
     public static void main(String[] args) {
        
         new Apemweb1();
         
     }
+    public menu()
+    {
+        this.Apemweb1();
 
+         int k = 1;
+        while (k != 0) {
+            String menu0Input = JOptionPane.showInputDialog(null, "Bienvenido al menusito, escoja la opcion que desee. \n"
+                    + "1. Suma \n"
+                    + "2. Resta \n"
+                    + "0. salir\n", JOptionPane.QUESTION_MESSAGE);
+
+            int menu0 = Integer.parseInt(menu0Input);
+            switch (menu0) 
+            {
+                case 1:
+                    this.suma();
+                    break;
+                case 2:
+                    this.resta();
+                    break;
+                case 0:
+                    System.exit(1);
+
+                    break;
+            }
+    }
   public Apemweb1() {
         a = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese primer digito"));
         b = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese segundo digito"));
 
         c = a + b;
-        JOptionPane.showMessageDialog(null, "El resultado es " + c);
+        resta = a-b;
+    }
+    public suma()
+    {
+       JOptionPane.showMessageDialog(null, "El resultado es " + c); 
+    }
+    public resta()
+    {
+       JOptionPane.showMessageDialog(null, "El resultado es " + resta);
     }
 }
